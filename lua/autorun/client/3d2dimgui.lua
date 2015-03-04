c97c126bc34721a1bea3c3b54847186e82595ff2
wyozi <joonasmertanen@gmail.com>
2015-03-04 22:07:45 +0200

    Pass self to queued functions

--- lua/autorun/client/3d2dimgui.lua
+++ lua/autorun/client/3d2dimgui.lua
@@ -272,7 +272,7 @@ end
 
 function tdui_meta:_QueueRender(fn)
 	if self._rendering then
-		local r, e = pcall(fn)
+		local r, e = pcall(fn, self)
 		if not r then print("TDUI rendering error: ", e) end
 		return
 	end
@@ -454,7 +454,7 @@ function tdui_meta:Render(pos, angles, scale)
 	self:BeginRender()
 
 		for i=1, #self.renderQueue do
-			local r, e = pcall(self.renderQueue[i])
+			local r, e = pcall(self.renderQueue[i], self)
 			if not r then print("TDUI rendering error: ", e) end
 		end
 
