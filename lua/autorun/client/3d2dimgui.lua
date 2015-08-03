f77dd0c466c3877ed1c9d7df32e91d36e5e30051
wyozi <joonasmertanen@gmail.com>
2015-08-03 17:20:19 +0300

    :bowtie:

--- lua/autorun/client/3d2dimgui.lua
+++ lua/autorun/client/3d2dimgui.lua
@@ -1,7 +1,7 @@
 --[[
 	The MIT License (MIT)
 
-	Copyright (c) 2014 Wyozi <http://www.github.com/wyozi>
+	Copyright (c) 2014-2015 Wyozi <http://www.github.com/wyozi>
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
